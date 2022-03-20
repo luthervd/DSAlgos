56f3797261766eefe05c3e5c72289c7244362f43
luthervd <luke.mcg79@gmail.com>
2022-03-20 21:12:13 +0000

    Symbol graph

---
+++ Graphs/SymbolGraph.cs
@@ -0,0 +1,39 @@
+﻿namespace Graphs
+{
+    public class SymbolGraph
+    {
+        public SymbolGraphReadResult Read(string filePath, string seperator)
+        {
+            var symbols = new Dictionary<string, int>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                var args = line.Split(seperator);
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (!symbols.ContainsKey(args[i]))
+                    {
+                        symbols.Add(args[i], symbols.Count() - 1);
+                    }
+                }
+            }
+            var keys = new string[symbols.Count];
+            foreach(var kv in symbols)
+            {
+                keys[kv.Value] = kv.Key;
+            }
+            var graph = new Graph(symbols.Count);
+            foreach(var line in lines)
+            {
+                var args = line.Split(seperator);
+                var v = symbols[args[0]];
+                for(int i = 0; i < args.Length; i++)
+                {
+                    graph.AddEdge(v, symbols[args[i]]);
+                }
+            }
+
+            return new SymbolGraphReadResult(symbols,keys,graph);
+        }
+    }
+}
---
+++ Graphs/SymbolGraphReadResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class SymbolGraphReadResult
+    {
+        private readonly Dictionary<string, int> _symbols;
+        private readonly string[] _keys;
+
+
+        public SymbolGraphReadResult(Dictionary<string, int> symbols, string[] keys, Graph graph)
+        {
+            _symbols = symbols;
+            _keys = keys;
+            Graph = graph;
+        }
+
+        public Graph Graph { get; private set; }
+
+        public bool Contains(string s) => _symbols.ContainsKey(s);
+
+        public int Index(string s) => _symbols[s];
+
+        public string Name(int v) => _keys[v];
+
+    }
+}
+
